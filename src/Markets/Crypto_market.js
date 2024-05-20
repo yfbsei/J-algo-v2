47942bf6eb762f19306df13f278eedd48d7adeb5
Jamallo <jamallo369@gmail.com>
2024-05-20 16:40:05 -0400

    Discord support, send trade signal to discord channel

--- src/Markets/Crypto_market.js
+++ src/Markets/Crypto_market.js
@@ -1,8 +1,8 @@
 import { WebSocket } from 'ws';
 import binance_candles from '../Util/util.js';
-import jTSv2 from '../Trend-sniper-v2/jTSv2.js';
-
 import send_signal from '../Social/Discord/discord-signal.js';
+
+import j_algo from '../j-algo.js';
 
 // TODO multiple coins and bybit
 
@@ -11,7 +11,8 @@ const [coin, timeframe, market, exchange] = ["BTCUSDT", "1m", "futures", "Binanc
 const
   baseURl = (market === "spot") ? "stream" : "fstream",
   candels = await binance_candles(market, coin, timeframe),
-  socket = new WebSocket(`wss://${baseURl}.binance.com/ws/btcusdt@kline_${timeframe}`);
+  socket = new WebSocket(`wss://${baseURl}.binance.com/ws/btcusdt@kline_${timeframe}`),
+  discord_channel = "https://discord.com/api/webhooks/1237128102308610140/-7U_KE7g7oJVfT1Wpx7f4dKYU1ljS5y03oSMVylJCdfyHiLMIQmGPifzHXdb0Qcn9r1n"; // webhook url
 
 socket.on('message', (event) => {
   const { k } = JSON.parse(event.toString());
@@ -24,21 +25,41 @@ socket.on('message', (event) => {
         candels[key].push(parseFloat(candle[i]));           
       }
        
-      const signal = jTSv2(candels);
+      const signal = j_algo(candels);
 
-      if(signal) send_signal({
+      if(signal.body?.entry) send_signal({
         avatar_url: 'https://yt3.googleusercontent.com/W5i3MAGlRSO-l3ykaKrWtieVp-hHJmufF4wZPxEEKsRz57LTXpLNsLw3gOITAJgLPb8KZ0uv=s160-c-k-c0x00ffffff-no-rj',
         username: 'J-algo',
         embeds: [{
-        title: `${coin} _ ${timeframe}inute _ ${market} _ ${exchange}`,
-        description: 
+        title: `${coin} ${timeframe}in ${market} ${exchange}`,
+        description:
         `
-        Order type: ${signal.order}
-        Position: ${signal.position}
-        Entry: ${signal.entry} 
+        Trade Id: ${signal.header.id}
+        Time stamp: ${signal.header.time_stamp}
+
+        *Order type: ${signal.header.order}*
+        *Position: \`${signal.header.position}\`*
+
+        **Entry: \`${signal.body.entry}\`**
+        **Stop loss: \`${signal.body.stop_loss}\`**
         `,
-        color: signal.position === "long" ? 0x00FF00 : 0xFF0000,
+        color: signal.header.position === "long" ? 0x00FF00 : 0xFF0000,
         //image: {url: ''},
-      }]}, "https://discord.com/api/webhooks/1237128102308610140/-7U_KE7g7oJVfT1Wpx7f4dKYU1ljS5y03oSMVylJCdfyHiLMIQmGPifzHXdb0Qcn9r1n"); // webhook url
+      }]}, discord_channel);
+
+      if(signal.body?.update_stop_loss) send_signal({
+        avatar_url: 'https://yt3.googleusercontent.com/W5i3MAGlRSO-l3ykaKrWtieVp-hHJmufF4wZPxEEKsRz57LTXpLNsLw3gOITAJgLPb8KZ0uv=s160-c-k-c0x00ffffff-no-rj',
+        username: 'J-algo',
+        embeds: [{
+          title: "Stop loss update",
+          description:
+          `
+          Trade Id: ${signal.header.id}
+
+          **New Stop loss: \`${signal.body.update_stop_loss}\`**
+          `,
+          color: 0x999999,
+        }]}, discord_channel);
     }
+
 });
